0846baa0c153475f2c1ed41dc8f781620fbd240b
tpgmartin <tpgmartin@gmail.com>
2021-06-06 21:47:20 +0100

    Update model used

--- run_detect.sh
+++ run_detect.sh
@@ -2,4 +2,4 @@
 
 FILEPATH=../ACE/ImageNet/ILSVRC2012_img_train/crop_jeep/img_sample/jeep
 
-python detect.py --source $FILEPATH --weights yolov5s.pt --conf 0.25 --accepted-names 'truck' 'train' 'car' --save-crop
+python detect.py --source $FILEPATH --weights yolov5l.pt --conf 0.25 --accepted-names 'truck' 'train' 'car' --save-crop

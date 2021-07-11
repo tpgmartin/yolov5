003ea696a30961b47a26801bc8c7aaeabd6b6807
tpgmartin <tpgmartin@gmail.com>
2021-07-11 17:50:36 +0100

    WIP find mask coords

--- utils/general.py
+++ utils/general.py
@@ -700,12 +700,33 @@ def apply_classifier(x, model, img, im0):
 def save_one_box(xyxy, im, file='image.jpg', gain=1.02, pad=10, square=False, BGR=False, save=True):
     # Save image crop as {file} with crop size multiple {gain} and {pad} pixels. Save and/or return crop
     xyxy = torch.tensor(xyxy).view(-1, 4)
+
+    print('xyxy')
+    print(xyxy)
+    print('------------')
     b = xyxy2xywh(xyxy)  # boxes
     if square:
         b[:, 2:] = b[:, 2:].max(1)[0].unsqueeze(1)  # attempt rectangle to square
     b[:, 2:] = b[:, 2:] * gain + pad  # box wh * gain + pad
+    print('b')
+    print(b)
+    print('------------')
     xyxy = xywh2xyxy(b).long()
     clip_coords(xyxy, im.shape)
+    print('img dim.', im.shape)
+    print('xyxy', xyxy)
+    mask = []
+    for i in range(im.shape[1]):
+        mask.append([])
+        for j in range(im.shape[0]):
+            mask.append(0)
+    
+    print(xyxy[0, 1])
+    print(xyxy[0, 3])
+    print(xyxy[0, 0])
+    print(xyxy[0, 2])
+
+
     crop = im[int(xyxy[0, 1]):int(xyxy[0, 3]), int(xyxy[0, 0]):int(xyxy[0, 2]), ::(1 if BGR else -1)]
     if save:
         path = str(increment_path(file, mkdir=True).with_suffix('.jpg'))
